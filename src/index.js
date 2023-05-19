a63c4a86fe4cef50e35c86c8730c1e90991c060f
IhorHodebskyi <121040851+IhorHodebskyi@users.noreply.github.com>
2023-05-19 21:37:27 +0300

    task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,56 @@
 import './css/styles.css';
-
+var debounce = require('lodash.debounce');
 const DEBOUNCE_DELAY = 300;
+import { fetchCountries } from './js/fetchCountries';
+const searchBox = document.getElementById('search-box');
+const countrylist = document.querySelector('.country-list');
+const countryInfo = document.querySelector('.country-info');
+searchBox.addEventListener('input', debounce(onInputSearch, DEBOUNCE_DELAY));
+
+function onInputSearch(e) {
+  e.preventDefault();
+
+  const value = searchBox.value.trim();
+  console.log(value);
+  fetchCountries(value)
+    .then(renderCountryList)
+    .catch(err => {
+      console.log(err);
+    });
+}
+
+function renderCountryList(e) {
+  const markup = renderCountryList
+    .map(({ name, capital, population, flags, languages }) => {
+      return `
+            
+              <img src="${flags.svg}" alt="${name.official} width="320" height="auto"">
+              <p><b>capital</b>: ${capital}</p>
+              <p><b>population</b>: ${population}</p>
+              <p><b>languages</b>: ${languages}</p>
+            
+        `;
+    })
+    .join('');
+  return (countrylist.innerHTML = markup);
+}
+
+// warning: {
+//   background: '#eebf31',
+//   textColor: '#fff',
+//   childClassName: 'warning',
+//   notiflixIconColor: 'rgba(0,0,0,0.2)',
+//   fontAwesomeClassName: 'fas fa-exclamation-circle',
+//   fontAwesomeIconColor: 'rgba(0,0,0,0.2)',
+//   backOverlayColor: 'rgba(238,191,49,0.2)', // v2.2.0 and the next versions
+// },
+
+// info: {
+//   background: '#26c0d3',
+//   textColor: '#fff',
+//   childClassName: 'info',
+//   notiflixIconColor: 'rgba(0,0,0,0.2)',
+//   fontAwesomeClassName: 'fas fa-info-circle',
+//   fontAwesomeIconColor: 'rgba(0,0,0,0.2)',
+//   backOverlayColor: 'rgba(38,192,211,0.2)', // v2.2.0 and the next versions
+// },
--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,20 +1,14 @@
-const BASE_URL = 'https://restcountries.com/v3.1/name';
-const serchParams = new URLSearchParams({
-  fields: 'name,capital,population,flags,languages,',
+const BASE_URL = 'https://restcountries.com/v3.1/name/';
+const searchParams = new URLSearchParams({
+    fields: 'name,capital,population,flags,languages,',
 });
-function fetchCountries(name) {
-  return fetch(`${BASE_URL}/${name}?${serchParams}`).then(response => {
-    if (!response.ok) {
-      throw new Error(response.status);
-    }
-    return response.json();
-  });
-}
 
-fetchCountries('deuts')
-  .then(data => {
-    console.log(data);
-  })
-  .catch(err => {
-    console.log(err);
-  });
+export const fetchCountries = (name) => {
+    return fetch(`${BASE_URL}${name}?${searchParams}`)
+        .then(response => {
+            if (response.status === 404) {
+                throw new Error(response.status);
+            }
+            return response.json();
+        });
+};
